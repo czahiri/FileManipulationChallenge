908b189beaf7a445aceab00d0159f931200554f3
Cyrus Zahiri <czahiri1@hwemail.com>
2025-09-15 12:30:44 -0700

    Initializes main directory creation
    Adds the creation of the main directory to
    setup the file manipulation challenge environment.

--- FileHandlingActivity.java
+++ FileHandlingActivity.java
@@ -3,9 +3,9 @@ import java.io.*;
 public class FileHandlingActivity {
     public static void main(String[] args) {
         // Your code here
-        
         // a. Create main directory
-        
+        File main = new File("/Users/cyruszahiri/Desktop/HTCS_Projects/FileManipulationChallenge");
+        main.mkdir();
         // b. Create three text files
         
         // c. Write messages to files
