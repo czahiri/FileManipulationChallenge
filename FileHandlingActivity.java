7b421f6a2563dc703f492321a8700fdf6c84d8b0
Cyrus Zahiri <czahiri1@hwemail.com>
2025-09-15 12:42:33 -0700

    Creates Backup Directory
    Creates a backup directory.
    
    This ensures files are created in the intended directory and prepares for future backup functionality.

--- FileHandlingActivity.java
+++ FileHandlingActivity.java
@@ -7,11 +7,11 @@ public class FileHandlingActivity {
         File main = new File("/Users/cyruszahiri/Desktop/HTCS_Projects/FileManipulationChallenge");
         main.mkdir();
         // b. Create three text files
-        File file = new File("myfile.txt");
+        File file = new File("/Users/cyruszahiri/Desktop/HTCS_Projects/FileManipulationChallenge/main");
         file.createNewFile();
-        File file2 = new File("myfile2.txt");
+        File file2 = new File("/Users/cyruszahiri/Desktop/HTCS_Projects/FileManipulationChallenge/main");
         file2.createNewFile();
-        File file3 = new File("myfile3.txt");
+        File file3 = new File("/Users/cyruszahiri/Desktop/HTCS_Projects/FileManipulationChallenge/main");
         file3.createNewFile();
         // c. Write messages to files
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
@@ -55,9 +55,10 @@ public class FileHandlingActivity {
             System.err.println("Error reading file: " + e.getMessage());
         }
         // e. Create backup directory
-        
+        File backupDir = new File("/Users/cyruszahiri/Desktop/HTCS_Projects/FileManipulationChallenge");
+        backupDir.mkdir();
         // f. Copy contents to backup file
-        
+  
         // g. List all files in both directories
     }
 }
