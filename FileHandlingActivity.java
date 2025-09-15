8ad061f47672c713cfe553f733fb236a53686aed
Cyrus Zahiri <czahiri1@hwemail.com>
2025-09-15 12:34:01 -0700

    Creates 3 text files
    Implements the creation of a main directory and three text files.
    
    This commit sets up the basic file structure required for subsequent operations.

--- FileHandlingActivity.java
+++ FileHandlingActivity.java
@@ -1,13 +1,18 @@
 import java.io.*;
 
 public class FileHandlingActivity {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Your code here
         // a. Create main directory
         File main = new File("/Users/cyruszahiri/Desktop/HTCS_Projects/FileManipulationChallenge");
         main.mkdir();
         // b. Create three text files
-        
+        File file = new File("myfile.txt");
+        file.createNewFile();
+        File file2 = new File("myfile2.txt");
+        file2.createNewFile();
+        File file3 = new File("myfile3.txt");
+        file3.createNewFile();
         // c. Write messages to files
         
         // d. Read and display file contents
